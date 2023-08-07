15e9d40f39b0734dae139026354c8be6307b3c05
paulwwstorm <paulwwstorm@gmail.com>
2023-08-07 15:29:28 -0500

    On the right path *hopefully*

--- QuickStart.java
+++ QuickStart.java
@@ -23,12 +23,43 @@ class Solution {
     // System.out.println(Arrays.toString(nums1));
     // }
 
+    // Still not working, but I think this is the right track
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        //Declare new array of size m + n
+        // Declare new array of size m + n
         int[] combinedArray = new int[m + n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = i; j < nums1.length; j++) {
+
+        int nums1Position = 0;
+        int nums2Position = 0;
+        int combinedArrayPosition = 0;
+
+        while (true) {
+            if (nums1[nums1Position] > nums2[nums2Position]) {
+                combinedArray[combinedArrayPosition] = nums1[nums1Position];
+                nums1Position++;
+                combinedArrayPosition++;
+                if (nums1Position == m) {
+                    for (int i = nums2Position; nums2Position > n; i++) {
+                        combinedArray[combinedArrayPosition] = nums2[i];
+                        combinedArrayPosition++;
+                    }
+                    break;
+                }
+            } else {
+                combinedArray[combinedArrayPosition] = nums2[nums2Position];
+                nums2Position++;
+                combinedArrayPosition++;
+                if (nums2Position == n) {
+                    for (int i = nums1Position; nums1Position > m; i++) {
+                        combinedArray[combinedArrayPosition] = nums1[i];
+                        combinedArrayPosition++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        nums1 = combinedArray;
+        System.out.println(Arrays.toString(nums1));
     }
 
     public static void main(String[] args) {
