ccf2b21c49cd5b3f4b75a72e99abf15726bf38b8
paulwwstorm <paulwwstorm@gmail.com>
2023-08-09 11:59:37 -0500

    Completed question 27

---
+++ LeetCode Top 150/27. Remove Elevement.java
@@ -0,0 +1,19 @@
+//Solution using System.arraycopy()
+class Solution {
+    public static int removeElement(int[] nums, int val) {
+        int remaining = nums.length;
+        for (int i = 0; i < remaining; i++) {
+            if (nums[i] == val) {
+                System.arraycopy(nums, i + 1, nums, i, nums.length - (i + 1));
+                i--;
+                remaining--;
+            }
+        }
+        return (remaining);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 4, 5, 3, 4, 1, 6, 4 };
+        System.out.println(removeElement(nums, 4));
+    }
+}
