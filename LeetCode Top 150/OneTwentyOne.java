4a26a517cc8460abac3b035d7955e4bf557d1060
paulwwstorm <paulwwstorm@gmail.com>
2023-08-21 11:07:03 -0500

    Not my proudest solution. But it does work.

--- LeetCode Top 150/OneTwentyOne.java
+++ LeetCode Top 150/OneTwentyOne.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import javax.sound.midi.SysexMessage;
+
 public class OneTwentyOne {
 
     // Double For Loops
@@ -43,35 +45,73 @@ public class OneTwentyOne {
     // return maxDiff;
     // }
 
-    public static int maxProfitThree(int[] prices) {
-        int maxDiff = 0;
-        int currentMinIndex = 0;
-        int currentMaxIndex = 0;
+    // public static int maxProfitThree(int[] prices) {
+    // int maxDiff = 0;
+    // int currentMinIndex = 0;
+    // int currentMaxIndex = 0;
 
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i] < prices[currentMinIndex] || i == 0) {
-                if ((i < currentMaxIndex)) {
-                    currentMinIndex = i;
-                    maxDiff = prices[currentMaxIndex] - prices[i];
-                } else {
-                    for (int j = i + 1; j < prices.length; j++) {
-                        if ((prices[j] - prices[i]) > 0 && (prices[j] - prices[i]) > maxDiff) {
-                            currentMaxIndex = j;
-                            currentMinIndex = i;
-                            maxDiff = prices[j] - prices[i];
-                        }
+    // for (int i = 0; i < prices.length - 1; i++) {
+    // if (prices[i] < prices[currentMinIndex] || i == 0) {
+    // if ((i < currentMaxIndex)) {
+    // currentMinIndex = i;
+    // maxDiff = prices[currentMaxIndex] - prices[i];
+    // } else {
+    // for (int j = i + 1; j < prices.length; j++) {
+    // if ((prices[j] - prices[i]) > 0 && (prices[j] - prices[i]) > maxDiff) {
+    // currentMaxIndex = j;
+    // currentMinIndex = i;
+    // maxDiff = prices[j] - prices[i];
+    // }
+    // }
+    // }
+
+    // if (prices[i] < prices[currentMinIndex]) {
+
+    // }
+    // }
+    // }
+
+    // return maxDiff;
+    // }
+
+    public static int maxProfitPointers(int[] nums) {
+        int maxDiff = 0;
+        int buyIndex = 0;
+        int sellIndex = 1;
+
+        while (sellIndex < nums.length) {
+            int currentDiff = nums[sellIndex] - nums[buyIndex];
+            // System.out.println(nums[buyIndex]);
+            // System.out.println(nums[buyIndex]);
+
+            if (currentDiff > maxDiff) {
+                maxDiff = currentDiff;
+                for (int i = buyIndex; i < sellIndex; i++) {
+                    int diff = nums[sellIndex] - nums[buyIndex];
+                    if (diff > maxDiff) {
+                        buyIndex = i;
+                        maxDiff = diff;
                     }
                 }
+            } else if (nums[sellIndex] < nums[buyIndex]) {
+                buyIndex = sellIndex - 1;
             }
+
+            sellIndex++;
         }
 
         return maxDiff;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 4, 5, 3, 4, 1, 6, 4 };
+        int[] nums1 = { 1, 2, 4, 5, 3, 4, 1, 6, 4 };
+        int[] nums2 = { 7, 1, 2, 4, 5, 3, 4, 1, 6, 4 };
+        int[] nums3 = { 5, 6, 7, 2, 3, 6 };
+
         // 1 1 2 3 4 4 4 5 6 i
         // 6 5 4 4 4 3 2 1 1 j
-        System.out.println(maxProfitThree(nums));
+        System.out.println(maxProfitPointers(nums1));
+        System.out.println(maxProfitPointers(nums2));
+        System.out.println(maxProfitPointers(nums3));
     }
 }
