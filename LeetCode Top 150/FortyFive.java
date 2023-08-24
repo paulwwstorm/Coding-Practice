cc6a3d9127ebf74b1cbc0c90ebbcf5e04c720d08
paulwwstorm <paulwwstorm@gmail.com>
2023-08-24 11:07:28 -0500

    Solved #45

--- LeetCode Top 150/FortyFive.java
+++ LeetCode Top 150/FortyFive.java
@@ -8,15 +8,19 @@ public class FortyFive {
             jumps++;
             while (position < nums.length - 2) {
                 if (position + currentJump >= (nums.length - 1)) {
-                    position += currentJump;
                     return jumps;
                 }
-                position++;
-                currentJump--;
-                if (currentJump < nums[position]) {
-                    jumps++;
-                    currentJump = nums[position];
+                jumps++;
+                int maxJumpPosition = position + 1;
+                int maxJump = nums[position + 1] + 1;
+                for (int i = 2; i <= currentJump; i++) {
+                    if ((nums[position + i] + i) > maxJump) {
+                        maxJumpPosition = position + i;
+                        maxJump = nums[position + i] + i;
+                    }
                 }
+                position = maxJumpPosition;
+                currentJump = nums[maxJumpPosition];
             }
         }
 
@@ -27,8 +31,13 @@ public class FortyFive {
         int[] nums1 = { 2, 3, 1, 1, 4 };
         int[] nums2 = { 2, 3, 0, 1, 4 };
         int[] nums3 = { 2, 3, 1 };
+        int[] nums4 = { 1, 2, 1, 1, 1 };
+        int[] nums5 = { 4, 1, 1, 3, 1, 1, 1 };
         System.out.println(jump(nums1));
         System.out.println(jump(nums2));
         System.out.println(jump(nums3));
+        System.out.println(jump(nums4));
+        System.out.println(jump(nums5));
+
     }
 }
