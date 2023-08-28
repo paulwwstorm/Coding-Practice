f27fb88dbf62532eab7bf4e4afdef6769c37b1a7
paulwwstorm <paulwwstorm@gmail.com>
2023-08-28 17:27:07 -0500

    Basic setup for #380

---
+++ LeetCode Top 150/ThreeEighty.java
@@ -0,0 +1,28 @@
+public class ThreeEighty {
+    public class RandomizedSet {
+
+        public RandomizedSet() {
+
+        }
+
+        public boolean insert(int val) {
+
+        }
+
+        public boolean remove(int val) {
+
+        }
+
+        public int getRandom() {
+
+        }
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
