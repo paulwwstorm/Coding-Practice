4b1d3675e14fdeed1b67316438b43aca16a4af4a
paulwwstorm <paulwwstorm@gmail.com>
2023-09-19 11:52:07 -0500

    Solved #14

--- LeetCode Top 150/Fourteen.java
+++ LeetCode Top 150/Fourteen.java
@@ -1,16 +1,24 @@
 public class Fourteen {
-    public String longestCommonPrefix(String[] strs) {
-        String longestCommonPrefix = "";
+    public static String longestCommonPrefix(String[] strs) {
+        int longestCommonPrefixEnd = 0;
 
         outerloop: for (int i = 0; i < strs[0].length(); i++) {
             for (int j = 0; j < strs.length; j++) {
-                if (strs[0].charAt(i) != strs[j].charAt(i)) {
+                if (strs[0].charAt(i) != strs[j].charAt(i) || i >= strs[j].length()) {
                     break outerloop;
                 }
             }
-            longestCommonPrefix = 
+            longestCommonPrefixEnd++;
         }
 
-        return longestCommonPrefix;
+        return strs[0].substring(0, longestCommonPrefixEnd);
+    }
+
+    public static void main(String[] args) {
+        String[] strs1 = { "flower", "flow", "flight" };
+        String[] strs2 = { "dog", "racecar", "car" };
+
+        System.out.println(longestCommonPrefix(strs1));
+        System.out.println(longestCommonPrefix(strs2));
     }
 }
