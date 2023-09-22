dc89261aa6211c8fd4d8a08575cb96f74c1db207
paulwwstorm <paulwwstorm@gmail.com>
2023-09-22 17:58:37 -0500

    Switching to Python, starting #6

--- LeetCode Top 150/Six.py
+++ LeetCode Top 150/Six.py
@@ -1,2 +1,4 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        # So what we want is start with the first element and then take the (n + (n - 1))th row
+        # then start with 
