7d3bd651344ecca61f50d3a875dd11fcaa4d7725
paulwwstorm <paulwwstorm@gmail.com>
2023-10-03 08:54:57 -0500

    Solved #125

--- LeetCode Top 150/Arrays and Strings/OneHundredTwentyFive.java
+++ LeetCode Top 150/Arrays and Strings/OneHundredTwentyFive.java
@@ -1,19 +1,17 @@
 class Solution {
     public static boolean isPalindrome(String s) {
-        String str = s.replaceAll("[^a-zA-Z]", "");
-        str.toLowerCase();
-
-        System.out.println(str);
+        String formattedStr = s.replaceAll("[^a-zA-Z0-9]", "");
+        formattedStr = formattedStr.toLowerCase();
 
         int pointerOne = 0;
-        int pointerTwo = str.length() - 1;
+        int pointerTwo = formattedStr.length() - 1;
 
         while (pointerOne < pointerTwo) {
-            if (s.charAt(pointerOne) != s.charAt(pointerTwo)) {
+            if (formattedStr.charAt(pointerOne) != formattedStr.charAt(pointerTwo)) {
                 return false;
             }
             pointerOne += 1;
-            pointerTwo += 1;
+            pointerTwo -= 1;
         }
 
         return true;
@@ -23,8 +21,10 @@ class Solution {
         String input1 = "A man, a plan, a canal: Panama";
         String input2 = "race a car";
         String input3 = " ";
+        String input4 = "0P";
         System.out.println(isPalindrome(input1));
         System.out.println(isPalindrome(input2));
         System.out.println(isPalindrome(input3));
+        System.out.println(isPalindrome(input4));
     }
 }
