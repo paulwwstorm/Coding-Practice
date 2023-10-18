5ad5cedd2e1c74117bda3fbe31f47c2dc74fec14
paulwwstorm <paulwwstorm@gmail.com>
2023-10-18 15:29:13 +0200

    Having issues with VSCode not finding classes. First attempt at solution.

--- LeetCode Top 150/Sliding Window/TwoHundredAndNine.java
+++ LeetCode Top 150/Sliding Window/TwoHundredAndNine.java
@@ -1,6 +1,38 @@
 public class TwoHundredAndNine {
     public static int minSubArrayLen(int target, int[] nums) {
-        return 0;
+        int length = 0;
+        int currSum = 0;
+        int arrBeginning = 0;
+        int arrEnd = 0;
+
+        forloop: for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            if (currSum >= target) {
+                length = arrEnd - arrBeginning + 1;
+                arrEnd = i;
+                break forloop;
+            }
+        }
+
+        while (arrEnd > nums.length) {
+            arrBeginning += 1;
+            arrEnd += 1;
+            currSum = currSum - nums[arrEnd] + nums[arrEnd];
+            if (currSum >= target) {
+                while (true) {
+                    if (currSum - nums[arrEnd + 1] >= target) {
+                        arrEnd += 1;
+                    } else {
+                        break;
+                    }
+                }
+                if ((arrEnd - arrBeginning + 1) < length) {
+                    length = arrEnd - arrBeginning + 1;
+                }
+            }
+        }
+
+        return length;
     }
 
     public static void main(String[] args) {
@@ -12,7 +44,7 @@ public class TwoHundredAndNine {
         int[] nums3 = { 1, 1, 1, 1, 1, 1, 1, 1 };
 
         System.out.println(minSubArrayLen(target1, nums1));
-        System.out.println(minSubArrayLen(target2, nums2));
-        System.out.println(minSubArrayLen(target3, nums3));
+        // System.out.println(minSubArrayLen(target2, nums2));
+        // System.out.println(minSubArrayLen(target3, nums3));
     }
 }
