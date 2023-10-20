839c4a8bac5ba5e0e0f678f9cebc8050a31b9936
paulwwstorm <paulwwstorm@gmail.com>
2023-10-20 10:59:54 +0200

    Initial setup.

---
+++ LeetCode Top 150/Hashmaps/ThreeHundredEigthyThree.java
@@ -0,0 +1,9 @@
+public class ThreeHundredEigthyThree {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
