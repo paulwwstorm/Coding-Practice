0de832b197f68549c9adf4e8f32f53af2bad1cb7
Paul Storm <paulwwstorm@gmail.com>
2024-02-02 07:38:53 +0800

    Yeah, getting there?

--- LeetCode Top 150/Stack/Twenty.java
+++ LeetCode Top 150/Stack/Twenty.java
@@ -1,16 +1,20 @@
 class Twenty {
+class Solution {
+class Solution {
     public boolean isValid(String s) {
         Stack<Character> charStack = new Stack<>();
-        List<Character> openCharacters = new ArrayList<>();
+        List<Character> openCharacters = new ArrayList<>(Arrays.asList("{".charAt(0), "[".charAt(0), "(".charAt(0)));
 
         for (int i = 0; i < s.length(); i++) {
+            System.out.println(i);
             if (openCharacters.contains(s.charAt(i))) {
                 charStack.push(s.charAt(i));
             } else if (
-                (s.charAt(i) == "]" & charStack.peek().equals("[")) | 
-                (s.charAt(i) == "}" & charStack.peek().equals("{")) | 
-                (s.charAt(i) == ")" & charStack.peek().equals("("))) {
-                    stringStack.pop();
+                charStack.size() != 0 &&
+                (("]".charAt(0) == s.charAt(i) && charStack.peek().equals("[")) | 
+                ("}".charAt(0) == s.charAt(i) && charStack.peek().equals("{")) | 
+                (")".charAt(0) == s.charAt(i) && charStack.peek().equals("(")))) {
+                    charStack.pop();
             } else {
                 return false;
             }
@@ -22,8 +26,9 @@ class Twenty {
 
         return true;
     }
-
+}
 
     public static void main(String[] args) {
+        System.out.println();
     }
 }
