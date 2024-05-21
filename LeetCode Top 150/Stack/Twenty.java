4b3730aad6f86543243cc598c729c60cb1c3fc57
Paul Storm <paulwwstorm@gmail.com>
2024-05-22 07:30:57 +0800

    Back on the horse, back on the stack?

--- LeetCode Top 150/Stack/Twenty.java
+++ LeetCode Top 150/Stack/Twenty.java
@@ -1,30 +1,36 @@
 class Twenty {
 class Solution {
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> charStack = new Stack<>();
-        List<Character> openCharacters = new ArrayList<>(Arrays.asList("{".charAt(0), "[".charAt(0), "(".charAt(0)));
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> charStack = new Stack<>();
+            List<Character> openCharacters = new ArrayList<>(Arrays.asList("{".charAt(0), "[".charAt(0), "(".charAt(0)));
 
-        for (int i = 0; i < s.length(); i++) {
-            System.out.println(i);
-            if (openCharacters.contains(s.charAt(i))) {
-                charStack.push(s.charAt(i));
-            } else if (
-                charStack.size() != 0 &&
-                (("]".charAt(0) == s.charAt(i) && charStack.peek().equals("[")) | 
-                ("}".charAt(0) == s.charAt(i) && charStack.peek().equals("{")) | 
-                (")".charAt(0) == s.charAt(i) && charStack.peek().equals("(")))) {
-                    charStack.pop();
-            } else {
+            if (s.length() % 2 != 0) {
                 return false;
             }
-        }
 
-        if (charStack.size() > 0) {
-            return false;
-        }
+            for (int i = 0; i < s.length()/2; i++) {
+                charStack.push(s.charAt(i));
+            }
 
-        return true;
+            for (int j = s.length()/2; j < s.length(); j++) {
+                System.out.println(s.charAt(j));
+                System.out.println(charStack.peek());
+                if (
+                    ("]".charAt(0) == s.charAt(j) && charStack.peek().equals("[")) | 
+                    ("}".charAt(0) == s.charAt(j) && charStack.peek().equals("{")) | 
+                    (")".charAt(0) == s.charAt(j) && charStack.peek().equals("("))
+                    ) {
+                        charStack.pop();
+                    }
+                else {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
 
