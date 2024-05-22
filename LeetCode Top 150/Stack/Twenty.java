d02d7d71a974096ae0c09a13c59e2a4a564fd619
Paul Storm <paulwwstorm@gmail.com>
2024-05-23 07:45:15 +0800

    This is what you try to pick up a problem after several months. Totally forgot that there might be multiple paranthesis

--- LeetCode Top 150/Stack/Twenty.java
+++ LeetCode Top 150/Stack/Twenty.java
@@ -4,27 +4,36 @@ class Solution {
     class Solution {
         public boolean isValid(String s) {
             Stack<Character> charStack = new Stack<>();
-            List<Character> openCharacters = new ArrayList<>(Arrays.asList("{".charAt(0), "[".charAt(0), "(".charAt(0)));
 
             if (s.length() % 2 != 0) {
                 return false;
             }
 
-            for (int i = 0; i < s.length()/2; i++) {
-                charStack.push(s.charAt(i));
+            for (int i = 0; i < s.length(); i++) {
+                if ("]".charAt(0) == s.charAt(i) | "]".charAt(0) == s.charAt(i) | "]".charAt(0) == s.charAt(i)) {
+                    charStack.push(s.charAt(i));
+                }
+                else {
+
+                }
             }
 
             for (int j = s.length()/2; j < s.length(); j++) {
                 System.out.println(s.charAt(j));
+                System.out.println(")".charAt(0) == s.charAt(j));
                 System.out.println(charStack.peek());
+                System.out.println(charStack.peek().equals("(".charAt(0)));
+                
                 if (
-                    ("]".charAt(0) == s.charAt(j) && charStack.peek().equals("[")) | 
-                    ("}".charAt(0) == s.charAt(j) && charStack.peek().equals("{")) | 
-                    (")".charAt(0) == s.charAt(j) && charStack.peek().equals("("))
+                    ("]".charAt(0) == s.charAt(j) && charStack.peek().equals("[".charAt(0))) | 
+                    ("}".charAt(0) == s.charAt(j) && charStack.peek().equals("{".charAt(0))) | 
+                    (")".charAt(0) == s.charAt(j) && charStack.peek().equals("(".charAt(0)))
                     ) {
+                        System.out.println("true");
                         charStack.pop();
                     }
                 else {
+                    System.out.println("false");
                     return false;
                 }
             }
