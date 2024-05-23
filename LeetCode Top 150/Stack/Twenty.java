494cf4139bebadac50995d2e2ad5d07ab915a7b6
Paul Storm <paulwwstorm@gmail.com>
2024-05-24 07:51:18 +0800

    Completed #20, not the most elegant solution. But it works and might be clear?

--- LeetCode Top 150/Stack/Twenty.java
+++ LeetCode Top 150/Stack/Twenty.java
@@ -2,45 +2,43 @@ class Twenty {
 class Solution {
 class Solution {
     class Solution {
-        public boolean isValid(String s) {
-            Stack<Character> charStack = new Stack<>();
+    class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> charStack = new Stack<>();
+        System.out.println(s);
 
-            if (s.length() % 2 != 0) {
-                return false;
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if ("(".charAt(0) == s.charAt(i) | "[".charAt(0) == s.charAt(i) | "{".charAt(0) == s.charAt(i)) {
+                charStack.push(s.charAt(i));
             }
-
-            for (int i = 0; i < s.length(); i++) {
-                if ("]".charAt(0) == s.charAt(i) | "]".charAt(0) == s.charAt(i) | "]".charAt(0) == s.charAt(i)) {
-                    charStack.push(s.charAt(i));
-                }
-                else {
-
-                }
-            }
-
-            for (int j = s.length()/2; j < s.length(); j++) {
-                System.out.println(s.charAt(j));
-                System.out.println(")".charAt(0) == s.charAt(j));
-                System.out.println(charStack.peek());
-                System.out.println(charStack.peek().equals("(".charAt(0)));
-                
+            else if (charStack.size() > 0) {
                 if (
-                    ("]".charAt(0) == s.charAt(j) && charStack.peek().equals("[".charAt(0))) | 
-                    ("}".charAt(0) == s.charAt(j) && charStack.peek().equals("{".charAt(0))) | 
-                    (")".charAt(0) == s.charAt(j) && charStack.peek().equals("(".charAt(0)))
+                    ("]".charAt(0) == s.charAt(i) && charStack.peek().equals("[".charAt(0))) | 
+                    ("}".charAt(0) == s.charAt(i) && charStack.peek().equals("{".charAt(0))) | 
+                    (")".charAt(0) == s.charAt(i) && charStack.peek().equals("(".charAt(0)))
                     ) {
-                        System.out.println("true");
                         charStack.pop();
                     }
                 else {
-                    System.out.println("false");
                     return false;
                 }
+            } else {
+                return false;
             }
+        }
 
+        if (charStack.size() == 0) {
             return true;
+        } else {
+            return false;
         }
     }
+}
+
 }
 
     public static void main(String[] args) {
