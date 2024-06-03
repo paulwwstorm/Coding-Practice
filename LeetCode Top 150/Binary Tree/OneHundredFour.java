406efe7f6af70a88d9d20e65be44935ad476f82f
Paul Storm <paulwwstorm@gmail.com>
2024-06-04 07:42:46 +0800

    Solved #104, not my worst one. A little clunky in execution, but solid logic.

--- LeetCode Top 150/Binary Tree/OneHundredFour.java
+++ LeetCode Top 150/Binary Tree/OneHundredFour.java
@@ -14,16 +14,24 @@ public class OneHundredFour {
         }
     }
 
-    public void traverseDepth(TreeNode node, int depth) {
+    public int traverseDepth(TreeNode node, int depth) {
+        int deepest = depth;
         if (node != null) {
-            traverseDepth(node.left, (depth + 1));
-            traverseDepth(node.right, (depth + 1));
-            System.out.println(" " + String.valueOf(node.val) + " " + String.valueOf(depth));
+            deepest += 1;
+            int depthLeft = traverseDepth(node.left, (depth + 1));
+            int depthRight = traverseDepth(node.right, (depth + 1));
+            if (depthLeft > deepest) {
+                deepest = depthLeft;
+            }
+            if (depthRight > deepest) {
+                deepest = depthRight;
+            }
         }
+
+        return deepest;
     }
 
     public int maxDepth(TreeNode root) {
-        traverseDepth(root, 0);
-        return 1;
+        return traverseDepth(root, 0);
     }
 }
