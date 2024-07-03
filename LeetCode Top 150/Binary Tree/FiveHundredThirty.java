8e5c85465c1a768dc0bc3c05a63f7b21adcbe087
Paul Storm <paulwwstorm@gmail.com>
2024-07-03 10:00:30 -0500

    #530 to-do: sort list and test pairs for smallest dif.

--- LeetCode Top 150/Binary Tree/FiveHundredThirty.java
+++ LeetCode Top 150/Binary Tree/FiveHundredThirty.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Solution {
     // Definition for a binary tree node.
     public class TreeNode {
@@ -13,7 +15,24 @@ class Solution {
         }
     }
 
+    public ArrayList<Integer> traverseDepth(TreeNode node, ArrayList<Integer> values) {
+        if (node != null) {
+            values.add(node.val);
+            values = traverseDepth(node.left, values);
+            values = traverseDepth(node.right, values);
+        }
+
+        return values;
+    }
+    
     public int getMinimumDifference(TreeNode root) {
-        
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        values = traverseDepth(root, values);
+
+        for (int i = 0; i < values.size(); i++) {
+            System.out.println(values.get(i));
+        }
+
+        return 1;
     }
 }
