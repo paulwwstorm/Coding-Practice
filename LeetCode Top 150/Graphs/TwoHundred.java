60e28b6513c1273a78b8b6bd9f4a069025731892
Paul Storm <paulwwstorm@gmail.com>
2024-07-08 15:14:34 -0500

    I have a general idea, first step record coordinates of all land spaces

--- LeetCode Top 150/Graphs/TwoHundred.java
+++ LeetCode Top 150/Graphs/TwoHundred.java
@@ -1,7 +1,19 @@
 public class TwoHundred {
     class Solution {
         public int numIslands(char[][] grid) {
-            
+            // System.out.println(String.valueOf(grid.length * grid[0].length));
+            char[] land = new char[(grid.length * grid[0].length)];
+            int landCount = 0;
+    
+            for (int i = 0; i < grid[0].length; i++) {
+                for (int j = 0; j < grid.length; j++) {
+                    if (char[j][i] == "1") {
+                        landCount++;
+                        land[landCount] = 
+                    }
+                }
+            }
+            return 1;
         }
     }
 }
