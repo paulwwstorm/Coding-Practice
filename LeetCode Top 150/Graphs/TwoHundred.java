114dc877f9fb186f7edcde4221804052ee50a8be
Paul Storm <paulwwstorm@gmail.com>
2024-07-09 13:03:35 -0500

    Progress on #200, but a lot to do.

--- LeetCode Top 150/Graphs/TwoHundred.java
+++ LeetCode Top 150/Graphs/TwoHundred.java
@@ -1,19 +1,35 @@
 public class TwoHundred {
     class Solution {
         public int numIslands(char[][] grid) {
-            // System.out.println(String.valueOf(grid.length * grid[0].length));
-            char[] land = new char[(grid.length * grid[0].length)];
+            int[][] land = new int[(grid.length * grid[0].length)][];
             int landCount = 0;
+            int islandCount = 0;
     
             for (int i = 0; i < grid[0].length; i++) {
                 for (int j = 0; j < grid.length; j++) {
-                    if (char[j][i] == "1") {
+                    if (grid[j][i] == "1".charAt(0)) {
+                        land[landCount] = new int[]{Character.getNumericValue(j),Character.getNumericValue(i)};
                         landCount++;
-                        land[landCount] = 
                     }
                 }
             }
-            return 1;
+    
+            System.out.println(String.valueOf(landCount));
+    
+            for (int k = 0; k < landCount; k++) {
+                int[] currentLand = land[k];
+                if (landCount == 1) {
+                    return islandCount++;
+                };
+                
+                if ((currentLand[0] == land[k+1][0] - 1 ) && (true)) {
+                    System.out.println("True");
+                } 
+    
+                landCount--;
+            }
+    
+            return islandCount;
         }
     }
 }
