1297b3ae03569e21e318c2643b962d290825f419
Paul Storm <paulwwstorm@gmail.com>
2024-11-27 11:32:08 -0600

    Wondering how to progress...

--- LeetCode Top 150/Graphs/NineHundredAndNine.java
+++ LeetCode Top 150/Graphs/NineHundredAndNine.java
@@ -1,3 +1,15 @@
 public class NineHundredAndNine {
-    
+    public int snakesAndLadders(int[][] board) {
+        int[] endpoint = {board.length, board[0].length};
+        int[] currLocation = {0,0};
+        int moveCounter = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) { 
+                
+            }
+        }
+
+        return 1;
+    }
 }
