40f47780324d3b7a8851d855b8108a023d8458d1
Paul Storm <paulwwstorm@gmail.com>
2024-11-29 10:53:50 -0600

    working towards a basic machanic

--- LeetCode Top 150/Graphs/NineHundredAndNine.java
+++ LeetCode Top 150/Graphs/NineHundredAndNine.java
@@ -1,12 +1,14 @@
 public class NineHundredAndNine {
     public int snakesAndLadders(int[][] board) {
-        int[] endpoint = {board.length, board[0].length};
-        int[] currLocation = {0,0};
+        int endpoint = board.length*board[0].length;
+        int currLocation = 1;
         int moveCounter = 0;
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) { 
-                
+        while (currLocation < endpoint) {
+            int farthestMove = 0;
+
+            for (int i = 1; i < 7; i++) {
+                int currentMove = board[board.length]
             }
         }
 
