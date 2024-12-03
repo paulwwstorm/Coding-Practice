0ce637e7a854d67a28e9e9ce820b7e9cae0283f8
Paul Storm <paulwwstorm@gmail.com>
2024-12-03 13:47:33 -0600

    Something is off here...

--- LeetCode Top 150/Graphs/NineHundredAndNine.java
+++ LeetCode Top 150/Graphs/NineHundredAndNine.java
@@ -6,10 +6,30 @@ public class NineHundredAndNine {
 
         while (currLocation < endpoint) {
             int farthestMove = 0;
+            System.out.println("This role: ");
+            
 
             for (int i = 1; i < 7; i++) {
-                int currentMove = board[board.length]
+                Boolean oddRow = (currLocation / board[0].length) % 2 == 0;
+                int currentMove = 0;
+                if (oddRow) {
+                    currentMove = board[currLocation / board[0].length][board[0].length - (((currLocation + i) % board[0].length) + 1)];
+                } else {
+                    currentMove = board[currLocation / board[0].length][(currLocation + i) % board[0].length];
+                }
+
+                if (currentMove < 0) {
+                    currentMove = currLocation + i;
+                }
+
+                if (currentMove > farthestMove) {
+                    currLocation = currentMove;
+                }
+                System.out.println(String.valueOf(currentMove));
             }
+
+            currLocation = currLocation + 6;
+            moveCounter++;
         }
 
         return 1;
