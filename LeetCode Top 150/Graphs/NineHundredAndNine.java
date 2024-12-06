901448ab279fe7159cde40caa653f8e7f0732989
Paul Storm <paulwwstorm@gmail.com>
2024-12-06 15:47:33 -0600

    Starting over with a different approach.

--- LeetCode Top 150/Graphs/NineHundredAndNine.java
+++ LeetCode Top 150/Graphs/NineHundredAndNine.java
@@ -4,38 +4,35 @@ public class NineHundredAndNine {
         int currLocation = 1;
         int moveCounter = 0;
 
-        while (currLocation < endpoint) {
-            int farthestMove = 0;
-            System.out.println("Current space: ");
-            System.out.println(String.valueOf(currLocation));
-            System.out.println("This roll: ");
-
-            for (int i = 1; i < 7; i++) {
-                Boolean oddRow = (currLocation / board[0].length) % 2 == 0;
-                int currentMove = 0;
-                if (oddRow) {
-                    currentMove = board[currLocation / board[0].length][board[0].length - (((currLocation + i) % board[0].length) + 1)];
-                } else {
-                    currentMove = board[currLocation / board[0].length][(currLocation + i) % board[0].length];
-                }
-
-                System.out.println(String.valueOf(currentMove));
-
-                if (currentMove < 0) {
-                    currentMove = currLocation + i;
-                }
-
-                if (currentMove > farthestMove) {
-                    farthestMove = currentMove;
-                }
-            }
-
-            System.out.println("Farthest move: ");
-            System.out.println(String.valueOf(farthestMove));
-            currLocation = farthestMove;
-            moveCounter++;
-        }
+        
 
         return moveCounter;
     }
 }
+
+// while (currLocation < endpoint) {
+//     int farthestMove = 0;
+//     System.out.println("Current space: ");
+//     System.out.println(String.valueOf(currLocation));
+//     System.out.println("This roll: ");
+//     for (int i = 1; i < 7; i++) {
+//         Boolean oddRow = (currLocation / board[0].length) % 2 == 0;
+//         int currentMove = 0;
+//         if (oddRow) {
+//             currentMove = board[currLocation / board[0].length][board[0].length - (((currLocation + i) % board[0].length) + 1)];
+//         } else {
+//             currentMove = board[currLocation / board[0].length][(currLocation + i) % board[0].length];
+//         }
+//         System.out.println(String.valueOf(currentMove));
+//         if (currentMove < 0) {
+//             currentMove = currLocation + i;
+//         }
+//         if (currentMove > farthestMove) {
+//             farthestMove = currentMove;
+//         }
+//     }
+//     System.out.println("Farthest move: ");
+//     System.out.println(String.valueOf(farthestMove));
+//     currLocation = farthestMove;
+//     moveCounter++;
+// }
