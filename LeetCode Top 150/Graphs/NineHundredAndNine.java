290613406a71ddb638f2e753178dad28c90561ad
Paul Storm <paulwwstorm@gmail.com>
2024-12-09 14:56:06 -0600

    there's a solution in there somewhere.

--- LeetCode Top 150/Graphs/NineHundredAndNine.java
+++ LeetCode Top 150/Graphs/NineHundredAndNine.java
@@ -1,33 +1,31 @@
 public class NineHundredAndNine {
-    public int snakesAndLadders(int[][] board) {
-        int endpoint = board.length*board[0].length;
-        int currLocation = 1;
-        int moveCounter = 0;
-
-        // So I clearly need something recursive here
-        // I think the thing I am concerned about is the end state
-        // So I think I want to write a function that that checks
-        // Whether or not the game can be won within one dice roll
-
+    class Solution {
         public int endCheck(int currLocation, int endpoint, int[][] board, int moveCounter) {
+            System.out.println("Endcheck initiated");
+            System.out.println(String.valueOf(currLocation));
             if (currLocation < endpoint && (currLocation + 6) >= endpoint) {
                 moveCounter++;
             } else {
-                
-                int bestMove = endpoint;
+                int bestMove = endCheck((currLocation + 6), endpoint, board, moveCounter + 1);
     
                 for (int i = 1; i < 7; i++) {
                     Boolean oddRow = (currLocation / board[0].length) % 2 == 0;
                     int currentMove = 0;
+                    int currentRoll = endpoint;
                     if (oddRow) {
                         currentMove = board[currLocation / board[0].length][board[0].length - (((currLocation + i) % board[0].length) + 1)];
+                        board[currLocation / board[0].length][board[0].length - (((currLocation + i) % board[0].length) + 1)] = -1;
                     } else {
                         currentMove = board[currLocation / board[0].length][(currLocation + i) % board[0].length];
+                        board[currLocation / board[0].length][(currLocation + i) % board[0].length] = -1;
                     }
-                    if (currentMove < 0) {
-                        currentMove = currLocation + i;
+    
+                    System.out.println(String.valueOf(currentMove));
+    
+                    if (currentMove > 0) {
+                        currentRoll = endCheck(currentMove, endpoint, board, moveCounter + 1);
                     }
-                    int currentRoll = endCheck(currentMove, endpoint, board, moveCounter);
+                    
                     if (currentRoll < bestMove) {
                         bestMove = currentRoll;
                     }
@@ -44,10 +42,15 @@ public class NineHundredAndNine {
             int currLocation = 1;
             int moveCounter = 0;
     
+            for (int i = 0; i < board.length / 2; i++) {
+                int[] t = board[i];
+                board[i] = board[board.length - 1 - i];
+                board[board.length - 1 - i] = t;
+            }
+    
+    
             return endCheck(currLocation, endpoint, board, moveCounter);
         }
-
-        return moveCounter;
     }
 }
 
