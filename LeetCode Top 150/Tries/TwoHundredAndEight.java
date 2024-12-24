5b17458e95232e9894cbd60e7d239094d4523a14
Paul Storm <paulwwstorm@gmail.com>
2024-12-24 10:19:16 -0600

    This totally original code that you definitely can't find anywhere else works super well.

--- LeetCode Top 150/Tries/TwoHundredAndEight.java
+++ LeetCode Top 150/Tries/TwoHundredAndEight.java
@@ -1,78 +1,74 @@
-package Tries;
+class TrieNode {
+    TrieNode[] children;
+    boolean terminalNode;
 
-public class TwoHundredAndEight {
-    class TrieNode {
-        TrieNode[] children;
-        boolean terminalNode;
-    
-        public void Trie() {
-            children = new TrieNode[26];
-    
-            terminalNode = false;
-    
-        }
+    public TrieNode() {
+        children = new TrieNode[26];
+
+        terminalNode = false;
+
     }
-    
-    class Trie {
-        private TrieNode root;
-    
-        public void Trie() {
-            root = new TrieNode();
-        }
-        
-        public void insert(String word) {
-            TrieNode currNode = root;
-    
-            for (int i = 0; i < word.length(); i++) {
-                char currChar = word.charAt(i);
-    
-                if (currNode.children[currChar - 'a'] == null) {
-                    currNode.children[currChar - 'a'] = new TrieNode();
-                }
-                currNode = currNode.children[currChar - 'a'];
-            }
-    
-            currNode.terminalNode = true;
-    
-        }
-        
-        public boolean search(String word) {
-            TrieNode currNode = root;
-    
-            for (int i = 0; i < word.length(); i++) {
-                char currChar = word.charAt(i);
-    
-                if (currNode.children[currChar - 'a'] == null) {
-                    return false;
-                }
-                currNode = currNode.children[currChar - 'a'];
-            }
-    
-            return currNode.terminalNode;
-        }
-        
-        public boolean startsWith(String prefix) {
-            TrieNode currNode = root;
-    
-            for (int i = 0; i < prefix.length(); i++) {
-                char currChar = prefix.charAt(i);
-    
-                if (currNode.children[currChar - 'a'] == null) {
-                    return false;
-                }
-                currNode = currNode.children[currChar - 'a'];
-            }
-    
-            return !currNode.terminalNode;
-        }
-    }
-    
-    /**
-        * Your Trie object will be instantiated and called as such:
-        * Trie obj = new Trie();
-        * obj.insert(word);
-        * boolean param_2 = obj.search(word);
-        * boolean param_3 = obj.startsWith(prefix);
-        */
-        
 }
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode currNode = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char currChar = word.charAt(i);
+
+            if (currNode.children[currChar - 'a'] == null) {
+                currNode.children[currChar - 'a'] = new TrieNode();
+            }
+            currNode = currNode.children[currChar - 'a'];
+        }
+
+        currNode.terminalNode = true;
+
+    }
+    
+    public boolean search(String word) {
+        TrieNode currNode = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char currChar = word.charAt(i);
+
+            if (currNode.children[currChar - 'a'] == null) {
+                return false;
+            }
+            currNode = currNode.children[currChar - 'a'];
+        }
+
+        return currNode.terminalNode;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode currNode = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char currChar = prefix.charAt(i);
+
+            if (currNode.children[currChar - 'a'] == null) {
+                return false;
+            }
+            currNode = currNode.children[currChar - 'a'];
+        }
+
+        return true;
+    }
+}
+
+/**
+    * Your Trie object will be instantiated and called as such:
+    * Trie obj = new Trie();
+    * obj.insert(word);
+    * boolean param_2 = obj.search(word);
+    * boolean param_3 = obj.startsWith(prefix);
+    */
+
