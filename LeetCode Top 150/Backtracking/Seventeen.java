27b4e9b42c9b6f02d59ceb9a736e2ee9dc2f381f
Paul Storm <paulwwstorm@gmail.com>
2024-12-27 16:02:25 -0600

    Definitely more complicated than I thought I first, but I feel like I have my head around the idea

--- LeetCode Top 150/Backtracking/Seventeen.java
+++ LeetCode Top 150/Backtracking/Seventeen.java
@@ -1,3 +1,27 @@
-public List<String> letterCombinations(String digits) {
+class Solution {
+    public Sttring letterBuilder(char[] letters, int digit, int marker) {
+        System.out.println(letters[digit][marker]);
+    
+        if (marker + 1 < letters[digit].length) {
+            String letterCombo = letterBuilder(letters, digit, (marker + 1));
+        }
+
+        return String.valueOf(letters[digit][marker]) + letterCombo ;
+    }
+
+    public List<String> letterCombinations(String digits) {
+        char[] letters = {
+            ["a","b","c"],
+            ["d","e","f"],
+            ["g","h","i"],
+            ["j","k","l"],
+            ["m","n","o"],
+            ["p","q","r","s"],
+            ["t","u","v"],
+            ["w","x","y"]
+            };
+
         
+        
+    }
 }
