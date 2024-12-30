630119484cda8fc81e9906a3733bd2227fb5c45e
Paul Storm <paulwwstorm@gmail.com>
2024-12-30 11:58:37 -0600

    Actually feel like I understand it, now working on implementation

--- LeetCode Top 150/Backtracking/Seventeen.java
+++ LeetCode Top 150/Backtracking/Seventeen.java
@@ -1,27 +1,60 @@
-class Solution {
-    public Sttring letterBuilder(char[] letters, int digit, int marker) {
-        System.out.println(letters[digit][marker]);
+public class Seventeen {
+    // I understand it now...
+    // famous last words
+    // The concept is that the whole string should be passed in
+    // The current number
+    // When the final number is reached
+    // It simply returns all letter options
+    // The next level up takes this output
+    // Adds all of its letter options
+    // And passes them up
+    // und so weiter
+
+    public static String[] letterBuilder(String[][] letters, int[] digits, int marker) {
     
-        if (marker + 1 < letters[digit].length) {
-            String letterCombo = letterBuilder(letters, digit, (marker + 1));
+        if (marker + 1 < digits.length) {
+            String[] letterOptions;
+            for (int j = 0; j < letters[marker].length; j++) {
+                letterOptions[j] = letters[marker][j];
+            }
+        } else {
+            String[] currentOptions = letterBuilder(letters, digits, marker + 1);
+            String[currentOptions.length*letters[marker].length] letterOptions;
+            for (int k = 0; k < letters[marker].length; k++) {
+                for (int l = 0; l < letterOptions.length; l++) {
+                    currentOptions[] = letters[marker][j];
+                }
+                letterOptions[k] = letters[marker][k];
+            }
         }
 
         return String.valueOf(letters[digit][marker]) + letterCombo ;
     }
 
-    public List<String> letterCombinations(String digits) {
-        char[] letters = {
-            ["a","b","c"],
-            ["d","e","f"],
-            ["g","h","i"],
-            ["j","k","l"],
-            ["m","n","o"],
-            ["p","q","r","s"],
-            ["t","u","v"],
-            ["w","x","y"]
-            };
+    // public void  List<String> letterCombinations(String digits) {
 
         
-        
+    // }
+
+    public static void main(String[] args) {
+        String[][] letters = {
+            {"a","b","c"},
+            {"d","e","f"},
+            {"g","h","i"},
+            {"j","k","l"},
+            {"m","n","o"},
+            {"p","q","r","s"},
+            {"t","u","v"},
+            {"w","x","y"}
+            };
+
+        int[] currentDial = {2};
+
+        String[] currentOptions = letterBuilder(letters, currentDial, 0);
+
+        for (int i = 0; i < currentOptions.length; i++) {
+            System.out.println(currentOptions[i]);
+        }
+    
     }
 }
