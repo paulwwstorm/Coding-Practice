19a62646d99c683235efa1256839eb252531f2d7
Paul Storm <paulwwstorm@gmail.com>
2025-01-14 11:22:01 -0600

    Testcases passed

--- LeetCode Top 150/Backtracking/Seventeen.java
+++ LeetCode Top 150/Backtracking/Seventeen.java
@@ -1,34 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Seventeen {
-    // I understand it now...
-    // famous last words
-    // The concept is that the whole string should be passed in
-    // The current number
-    // When the final number is reached
-    // It simply returns all letter options
-    // The next level up takes this output
-    // Adds all of its letter options
-    // And passes them up
-    // und so weiter
+class Solution {
     public static List<String> letterBuilder(String[][] letters, int[] digits, int marker) {
         List<String> letterOptions = new ArrayList<>();
-        if (marker + 1 == digits.length) {
-            for (int j = 0; j < letters[marker].length; j++) {
-                letterOptions.add(letters[marker][j]);
+        if (marker + 1 >= digits.length) {
+            for (int j = 0; j < letters[digits[marker] - 2].length; j++) {
+                letterOptions.add(letters[digits[marker] - 2][j]);
             }
+            // System.out.println(Arrays.toString(letterOptions.toArray()));
         } else {
-            List<String> currentOptions = letterBuilder(letters, digits, marker + 1);
-            for (int k = 0; k < letters[digits[marker]].length; k++) {
+            letterOptions = letterBuilder(letters, digits, marker + 1);
+            List<String> newLetterOptions = new ArrayList<>();
+            // System.out.println(String.valueOf(digits[marker]));
+            // System.out.println(Arrays.toString(letterOptions.toArray()));
+            for (int k = 0; k < letters[digits[marker] - 2].length; k++) {
+                String currLetter = letters[digits[marker] - 2][k];
+                // System.out.println(currLetter);
                 for (int l = 0; l < letterOptions.size(); l++) {
-                    currentOptions.add(letters[marker][k]);
+                    newLetterOptions.add(currLetter + letterOptions.get(l));
+                    System.out.println(currLetter + letterOptions.get(l));
                 }
-                letterOptions.add(letters[marker][k]);
+                // letterOptions.add(letters[marker][k]);
             }
 
             if (marker == 0) {
-                return letterOptions;
+                return newLetterOptions;
             }
         }
 
@@ -53,7 +47,11 @@ public class Seventeen {
             currentDial[i] = digits.charAt(i) - '0';
         }
 
-        List<String> currentOptions = letterBuilder(letters, currentDial, 0);
+        List<String> currentOptions = new ArrayList<>();
+
+        if (digits.length() != 0) {
+            currentOptions = letterBuilder(letters, currentDial, 0);
+        }
 
         return currentOptions;
         
