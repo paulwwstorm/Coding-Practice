4bd1e76f2b4b3e6af35fd227178666ecf476493b
Paul Storm <paulwwstorm@gmail.com>
2025-01-24 11:43:23 -0600

    This may be the worst code I have ever written. But #53 solved.

--- LeetCode Top 150/Kadane's Algorithm/FiftyThree.java
+++ LeetCode Top 150/Kadane's Algorithm/FiftyThree.java
@@ -1,13 +1,40 @@
 public class FiftyThree {
     class Solution {
         public int maxSubArray(int[] nums) {
-            int max;
-            int curr;
-
+            int max = nums[0];
+            int curr = 0;
+            boolean allNeg = true;
+    
             for (int i = 0; i < nums.length; i++) {
-                if ()
+                if (allNeg == true && nums[i] >= 0) {
+                    allNeg = false;
+                }
+    
+                if (nums[i] < 0 && nums[i] > max) {
+                    max = nums[i];
+                } else {
+                    if (curr + nums[i] > 0) {
+                        if (curr > max) {
+                            max = curr;
+                        }
+                        curr += nums[i];
+                    } else if (curr + nums[i] <= 0) {
+                        if (curr > max && allNeg == false) {
+                            max = curr;
+                        }
+    
+                        curr = 0;
+                    }
+    
+                    if (curr > max && allNeg == false) {
+                        max = curr;
+                    }
+                }
+    
+                System.out.println(String.valueOf(curr));
             }
-            
+    
+            return max;
         }
     }
 }
