e5980772d9e9f45ccb585be2c4e7f121aaa584ea
Paul Storm <paulwwstorm@gmail.com>
2025-01-28 14:35:24 -0600

    Starting binary insert search and writing out solution.

---
+++ LeetCode Top 150/Binary Search/ThirtyFive.java
@@ -0,0 +1,22 @@
+package Binary Search;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //If more than one element in array
+        //Take middle element
+        //If not target:
+        //If element is larger than target
+        //Take right side of array
+        //If smaller
+        //Take left side
+
+        //If only one element
+        //And not target
+        //If element larger
+        //Insert index to left
+        //Otherwise
+        //Insert index to the left
+
+        //I think this will work...
+    }
+}
