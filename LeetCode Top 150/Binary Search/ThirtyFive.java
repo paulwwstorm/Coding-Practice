899a9f40988a0f24e207600accab4b2e6f551bf8
Paul Storm <paulwwstorm@gmail.com>
2025-01-30 13:01:54 -0600

    Gotta use pointers

--- LeetCode Top 150/Binary Search/ThirtyFive.java
+++ LeetCode Top 150/Binary Search/ThirtyFive.java
@@ -1,22 +1,34 @@
-package Binary Search;
-
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        //If more than one element in array
-        //Take middle element
-        //If not target:
-        //If element is larger than target
-        //Take right side of array
-        //If smaller
-        //Take left side
+    public int insertSearchHelper(int[] nums, int target, int leftPointer, int rightPointer) {
+        int midPoint = (rightPointer-leftPointer)/2;
+        int midValue = nums[]
 
-        //If only one element
-        //And not target
-        //If element larger
-        //Insert index to left
-        //Otherwise
-        //Insert index to the left
+        if (midPoint > 0) {
 
-        //I think this will work...
-    }
+        } 
+        if (nums.length > 1) {
+            int midValue = nums[(nums.length/2) + 1];
+            if (midValue == target) {
+                return (nums.length/2) + 1;
+            } else if (target < midValue) {
+                int[] nextSearch = Arrays.copyOfRange(nums, 0, nums.length/2);
+            } else if (target > midValue) {
+                int nextSearch = Arrays.copyOfRange(nums,((nums.legnth/2) + 2), nums.length);
+            }
+        }
+        
+
+
+    //If only one element
+    //And not target
+    //If element larger
+    //Insert index to left
+    //Otherwise
+    //Insert index to the left
+
+    //I think this will work...
+}
+
+public int searchInsert(int[] nums, int target) {
+    return insertSearchHelper(nums, target, 0, (nums.length -1));
 }
