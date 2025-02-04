91a7e8f2582e7d4836fe466c50096890fbae5504
Paul Storm <paulwwstorm@gmail.com>
2025-02-04 09:20:34 -0600

    Lots of learning about heaps.

--- LeetCode Top 150/Heaps/TwoHundredFifteen.java
+++ LeetCode Top 150/Heaps/TwoHundredFifteen.java
@@ -1,5 +1,43 @@
 class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public int findKthLargest(int[] nums, int k) {
+        TreeNode root;
+        TreeNode currNode = root;
+        TreeNode prevNode; 
+        int pointer = 0;
+
+        while (nums.length > 0) {
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 0; i < nums.length, i++) {
+                if (nums[i] > max) {
+                    max = nums[i];
+                }
+            }
+
+            for (int j = 0; j < nums.length, j++) {
+                if (nums[j] == max) {
+                    prevNode = currNode
+                    currNode = 
+                    TreeNode newNode;
+                    currNode.val = nums[j];
+                    nums = ArrayUtils.removeElement(num, nums[j]);
+                    j = nums.length + 1;
+                }
+            }
+        }
         
     }
 }
