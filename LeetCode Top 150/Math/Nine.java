a5398225fe47d3117b29d4a64d9b6b2567b8e69b
Paul Storm <paulwwstorm@gmail.com>
2025-02-19 10:03:46 -0600

    Trouble with zero0

--- LeetCode Top 150/Math/Nine.java
+++ LeetCode Top 150/Math/Nine.java
@@ -1,11 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        System.out.println(String.valueOf(x / ((int) Math.pow(10, length - 2))));
+        System.out.println(String.valueOf(x % ((int) Math.pow(10, 2))));
         if (x > 9) {
             int length = (int) (Math.log10(x) + 1);
+            
             for (int i = 1; i <= length/2; i++) {
-                // System.out.println(String.valueOf(x / ((int) Math.pow(10, length - i))));
+                // System.out.println(String.valueOf(x / ((int) Math.pow(10, length - i))));\
+                // System.out.println(String.valueOf(i));
                 int first = x / ((int) Math.pow(10, length - i));
+                // System.out.println(String.valueOf(x / ((int) Math.pow(10, length - 2))));
                 int second = x % (int) Math.pow(10, i);
+                // System.out.println(String.valueOf(x % (int) Math.pow(10, 2));
                 if (first != second) {
                     return false;
                 }
