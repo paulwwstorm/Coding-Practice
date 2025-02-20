cde79f4d98bcbd5b93da196037e079b7491df9d3
Paul Storm <paulwwstorm@gmail.com>
2025-02-20 10:52:30 -0600

    Proof that if you keep mangling your code, eventually it works.

--- LeetCode Top 150/Math/Nine.java
+++ LeetCode Top 150/Math/Nine.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        System.out.println(String.valueOf(x / ((int) Math.pow(10, length - 2))));
-        System.out.println(String.valueOf(x % ((int) Math.pow(10, 2))));
         if (x > 9) {
             int length = (int) (Math.log10(x) + 1);
             
             for (int i = 1; i <= length/2; i++) {
-                // System.out.println(String.valueOf(x / ((int) Math.pow(10, length - i))));\
-                // System.out.println(String.valueOf(i));
-                int first = x / ((int) Math.pow(10, length - i));
-                // System.out.println(String.valueOf(x / ((int) Math.pow(10, length - 2))));
-                int second = x % (int) Math.pow(10, i);
-                // System.out.println(String.valueOf(x % (int) Math.pow(10, 2));
-                if (first != second) {
+                int frontFirst = (x % (int) Math.pow(10, (length + 1) - i)) - (x % (int) Math.pow(10, length - i));
+                int front = frontFirst / ((int) Math.pow(10, length - i));
+
+                int backFirst = (x % (int) Math.pow(10, i)) - (x % (int) Math.pow(10, i - 1));
+                int back = backFirst / ((int) Math.pow(10, i - 1));
+                                         
+                if (front != back) {
                     return false;
                 }
             }
