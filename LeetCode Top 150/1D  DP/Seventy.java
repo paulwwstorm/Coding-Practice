f832469ac7052b9beba68a2e60fe22812305ad48
Paul Storm <paulwwstorm@gmail.com>
2025-02-24 10:37:01 -0600

    This one hurts my brain a bit

--- LeetCode Top 150/1D  DP/Seventy.java
+++ LeetCode Top 150/1D  DP/Seventy.java
@@ -1,5 +1,22 @@
 class Solution {
+    int[] calculated = new int[1000];
+
+    public int stepCounter(int n){
+        if (n < 2) {
+            return n;
+        }
+
+        if (calculated[n] != 0) {
+            return calculated[n];
+        }
+
+        else {
+            calculated[n] = stepCounter(n - 1) + stepCounter(n - 2);
+            return calculated[n];
+        }
+    }
+
     public int climbStairs(int n) {
-        
+        return stepCounter(n + 1);
     }
 }
