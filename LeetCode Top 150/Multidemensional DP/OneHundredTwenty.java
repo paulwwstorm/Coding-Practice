2a267da89e93041d18dc8cf7ac8f01f0b604f6a5
Paul Storm <paulwwstorm@gmail.com>
2025-03-03 11:02:16 -0600

    Needs to be done recursively taking the shortest option each time.

---
+++ LeetCode Top 150/Multidemensional DP/OneHundredTwenty.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] values = {0,0,0,0};
+        for (int i = 0; i < triangle.length(); i++) {
+            System.out.printlm(String.valueOf(triangle[i][values[i]]));
+        }
+    }
+}
