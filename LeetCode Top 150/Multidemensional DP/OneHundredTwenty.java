4fa7092db032f953dac105dcb79ad45bc3260417
Paul Storm <paulwwstorm@gmail.com>
2025-03-05 07:34:34 -0600

    Well, I did bottom-up and top-down and the time limit is still exceeded. Time for more reading.

--- LeetCode Top 150/Multidemensional DP/OneHundredTwenty.java
+++ LeetCode Top 150/Multidemensional DP/OneHundredTwenty.java
@@ -21,3 +21,26 @@ class Solution {
 
     }
 }
+
+class Solution {
+    int smallestPath = Integer.MAX_VALUE;
+
+    public void pathfinder(List<List<Integer>> triangle, int row, int position, int currTotal) {
+            currTotal += triangle.get(row).get(position);
+            if (row + 1 == triangle.size()) {
+                if (currTotal < smallestPath) {
+                    smallestPath = currTotal;
+                }
+            } else {
+                pathfinder(triangle, row + 1, position, currTotal);
+                pathfinder(triangle, row + 1, position + 1, currTotal);
+            }
+
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        pathfinder(triangle, 0, 0, 0);
+        return smallestPath;
+
+    }
+}
