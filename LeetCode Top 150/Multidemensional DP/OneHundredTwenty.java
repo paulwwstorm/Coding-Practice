04c46706484498c10166349c53b454fc848fe871
Paul Storm <paulwwstorm@gmail.com>
2025-03-06 11:25:22 -0600

    Logic seems to be there, but code is buggy.

--- LeetCode Top 150/Multidemensional DP/OneHundredTwenty.java
+++ LeetCode Top 150/Multidemensional DP/OneHundredTwenty.java
@@ -44,3 +44,33 @@ class Solution {
 
     }
 }
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[][] paths = new int[triangle.size()][0];
+        int[] firstRow = new int[1];
+        firstRow[0] = triangle.get(0).get(0);
+        paths[0] = firstRow;
+
+        for (int i = 1; i < triangle.size(); i++) {
+            int[] row = new int[triangle.get(i).size()];
+            paths[i] = row;
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+                if (j > 0) {
+                    right = paths[i-1][j - 1];
+                }
+                
+                if (j < paths[i-1].length - 1) {
+                    left = paths[i-1][j];
+                }
+                int shortest = right < left ? right : left;
+                paths[i][j] = shortest + triangle.get(i).get(j);
+            }
+        }
+
+        return Arrays.stream(paths[paths.length - 1]).min().getAsInt();
+
+    }
+}
