1e5c1caaed6129238d19f74f14db5edea74c0166
Paul Storm <paulwwstorm@gmail.com>
2025-03-12 07:51:42 -0500

    Verily, verily I say to you: it's nice to not get my butt kicked.

--- LeetCode Top 150/Two Pointers/ThreeHundredNinetyTwo.java
+++ LeetCode Top 150/Two Pointers/ThreeHundredNinetyTwo.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
+        int currChar = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (currChar < s.length() && t.charAt(i) == s.charAt(currChar)) {
+                currChar++;
+            }
+        }
+
+        return currChar == s.length();
     }
 }
