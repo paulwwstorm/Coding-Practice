362ab14e5fe805d29a27f4614c3cc9ca818cf28e
Paul Storm <paulwwstorm@gmail.com>
2025-03-18 08:53:55 -0700

    Falling when the string contains nur a space.

--- LeetCode Top 150/Sliding Window/Three.java
+++ LeetCode Top 150/Sliding Window/Three.java
@@ -1,5 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        List<Character> currSubstring = new ArrayList<Character>();
+        int longest = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (currSubstring.indexOf(s.charAt(i)) >= 0) {
+                if (currSubstring.size() > longest) {
+                    longest = currSubstring.size();
+                }
+
+                currSubstring = new ArrayList<Character>();
+                currSubstring.add(s.charAt(i));
+            } else {
+                currSubstring.add(s.charAt(i));
+            }
+        }
+
+        return longest;
     }
 }
