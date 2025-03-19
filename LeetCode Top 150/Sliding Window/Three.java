24197effdcae67488217cfed7c562c6903ac2ead
Paul Storm <paulwwstorm@gmail.com>
2025-03-19 07:30:35 -0700

    SOlved #3, would be better to save results instead of starting over at next character.

--- LeetCode Top 150/Sliding Window/Three.java
+++ LeetCode Top 150/Sliding Window/Three.java
@@ -2,6 +2,7 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         List<Character> currSubstring = new ArrayList<Character>();
         int longest = 0;
+        int currStart = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (currSubstring.indexOf(s.charAt(i)) >= 0) {
@@ -10,9 +11,14 @@ class Solution {
                 }
 
                 currSubstring = new ArrayList<Character>();
+                currStart = currStart + 1;
+                i = currStart;
                 currSubstring.add(s.charAt(i));
             } else {
                 currSubstring.add(s.charAt(i));
+                if (currSubstring.size() > longest) {
+                    longest = currSubstring.size();
+                }
             }
         }
 
