b56118392cc5b1173ff9a3fac6564a83b2781f36
Paul Storm <paulwwstorm@gmail.com>
2025-04-24 15:25:08 +0200

    Basic solution for #205

--- LeetCode Top 150/Hashmaps/TwoHundredAndFive.java
+++ LeetCode Top 150/Hashmaps/TwoHundredAndFive.java
@@ -1,5 +1,25 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
+        Map<Character, Integer> sCharMap = new HashMap<>();
+        String sAsDigits = "";
+        Map<Character, Integer> tCharMap = new HashMap<>();
+        String tAsDigits = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sCharMap.get(s.charAt(i)) == null) {
+                sCharMap.put(s.charAt(i), i);
+            }
+            sAsDigits = sAsDigits + "-" + sCharMap.get(s.charAt(i));
+
+            if (tCharMap.get(t.charAt(i)) == null) {
+                tCharMap.put(t.charAt(i), i);
+            }
+            tAsDigits = tAsDigits + "-" + tCharMap.get(t.charAt(i));
+        }
+
+        System.out.println(sAsDigits);
+        System.out.println(tAsDigits);
+
+        return sAsDigits.equals(tAsDigits);
     }
 }
