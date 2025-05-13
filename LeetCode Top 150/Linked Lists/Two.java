f65d6015090826e6498ec191ff1ff127f89745da
Paul Storm <paulwwstorm@gmail.com>
2025-05-13 17:12:56 +0200

    Solved #2, can't wait to see how someone who knows what they are doing does it...

--- LeetCode Top 150/Linked Lists/Two.java
+++ LeetCode Top 150/Linked Lists/Two.java
@@ -10,6 +10,47 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+        ListNode sum = new ListNode();
+        Boolean firstNode = true;
+        ListNode currNode = sum;
+        ListNode currNode1 = l1;
+        ListNode currNode2 = l2;
+        int currCarry = 0;
+
+        while (currNode1 != null || currNode2 != null) {
+            ListNode newNode;
+            if (firstNode) {
+                newNode = sum;
+                firstNode = false;
+            } else {
+                newNode = new ListNode();
+                currNode.next = newNode;
+                currNode = newNode;
+            }
+
+            int digit1 = currNode1 != null ? currNode1.val : 0;
+            int digit2 = currNode2 != null ? currNode2.val : 0;
+
+            int currSum = digit1 + digit2 + currCarry;
+
+            if (currSum < 10) {
+                currCarry = 0;
+                newNode.val = currSum;
+            } else {
+                currCarry = 1;
+                newNode.val = currSum - 10;
+            }
+
+            currNode1 = currNode1 != null && currNode1.next != null ? currNode1.next : null;
+            currNode2 = currNode2 != null && currNode2.next != null ? currNode2.next : null;
+        }
+
+        if (currCarry > 0) {
+            ListNode newNode = new ListNode();
+            currNode.next = newNode;
+            newNode.val = currCarry;
+        }
+
+        return sum;
     }
 }
