3e05d7cb27bb13c482e8ac3e05559edf0834d5f0
Paul Storm <paulwwstorm@gmail.com>
2025-06-12 17:43:10 +0200

    Continuing #130

--- LeetCode Top 150/Graphs/OneHundredThirty.java
+++ LeetCode Top 150/Graphs/OneHundredThirty.java
@@ -1,5 +1,14 @@
 class Solution {
     public void solve(char[][] board) {
-        
+        int[] currLoc = {0,0};
+        boolean touchEdge = false;
+        boolean connected = false;
+        if (currLoc[0] - 1 < 0 ||
+            currLoc[0] + 1 >= board[0].length ||
+            currLoc[1] - 1 < 0 ||
+            currLoc[1] + 1 >= board[1].length
+        ) {
+            touchEdge = true;
+        } else ()
     }
 }
