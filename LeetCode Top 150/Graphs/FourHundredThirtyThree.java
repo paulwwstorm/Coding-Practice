6280dcbbe44800dd3fa9ef1845685971a1371e18
Paul Storm <paulwwstorm@gmail.com>
2025-06-25 16:27:09 +0800

    Yeah, this ain't the way...

---
+++ LeetCode Top 150/Graphs/FourHundredThirtyThree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int changeOne(String startGene, String endGene, String[] bank, int loc) {
+        if (startGene.charAt(loc) != )
+        String testGene = startGene.substring(0, loc) + endGene.charAt(loc) + startGene.substring(loc+1, startGene.length());
+
+        for (int i = 0; i < bank.lenght; i++) {
+
+        }
+    }
+
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        
+    }
+}
