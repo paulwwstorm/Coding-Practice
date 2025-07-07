42708f73ae1484fa9203ee36e4d49516171af67d
Paul Storm <paulwwstorm@gmail.com>
2025-07-07 16:27:27 +0800

    Solved #430. Disappointed in my failure to correctly understand instructions, encouraged by my ability to update the code.

--- LeetCode Top 150/Graphs/FourHundredThirtyThree.java
+++ LeetCode Top 150/Graphs/FourHundredThirtyThree.java
@@ -1,19 +1,14 @@
 class Solution {
-    public List<String> generateMutations(String startGene, String endGene, List<String> newOptions, String[] bank, List<String> visited, int loc) {
-        if (startGene.charAt(loc) != endGene.charAt(loc)) {
-            System.out.println("Diff found");
-            int end = newOptions.size();
-            for (int j = 0; j < end; j++) {
-                String mutation = newOptions.get(j).substring(0,loc) + endGene.charAt(loc) + newOptions.get(j).substring(loc+1, startGene.length());
-                newOptions.add(mutation);
-            }
+    public List<String> generateMutations(String startGene, String[] genes, List<String> newOptions, int loc) {
+        for (int f = 0; f < genes.length; f++) {
+            newOptions.add(startGene.substring(0,loc) + genes[f] + startGene.substring(loc+1, startGene.length()));
         }
         
         if (loc+1 == startGene.length()) {
             return newOptions;
         } else {
             loc++;
-            return generateMutations(startGene, endGene, newOptions, bank, visited, loc);
+            return generateMutations(startGene, genes, newOptions, loc);
         }
     }
 
@@ -21,14 +16,15 @@ class Solution {
         List<String> visited = new ArrayList<String>();
         List<String> currentLevel = Collections.singletonList(startGene);
         List<String> previousLevel = new ArrayList<String>();
-        int mutationCount = 0;
+        String[] genes = {"A", "C", "G", "T"};
+        int level = 0;
         boolean next = true;
 
         while(next) {
             for (int k = 0; k < currentLevel.size(); k++) {
                 if (currentLevel.get(k).equals(endGene)) return level;
                 visited.add(currentLevel.get(k));
-                System.out.println(currentLevel.get(k));
+                // System.out.println(currentLevel.get(k));
             }
             
             level++;
@@ -36,10 +32,10 @@ class Solution {
             currentLevel = new ArrayList<String>();
             for (int i = 0; i < previousLevel.size(); i++) {
                 List<String> newOptions = new ArrayList<String>();
-                newOptions.add(previousLevel.get(i));
-                newOptions = generateMutations(previousLevel.get(i), endGene, newOptions, bank, visited, 0);
+                // newOptions.add(previousLevel.get(i));
+                newOptions = generateMutations(previousLevel.get(i), genes, newOptions, 0);
                 for (int m = 0; m < newOptions.size(); m++) {
-                    System.out.println(newOptions.get(m));
+                    // System.out.println(newOptions.get(m));
                     for (int n = 0; n < bank.length; n++) {
                         if (bank[n].equals(newOptions.get(m)) && !visited.contains(newOptions.get(m))) {
                             currentLevel.add(newOptions.get(m));
