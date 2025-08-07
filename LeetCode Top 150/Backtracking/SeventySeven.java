420ac2a9d8dc904d7f0e17e57bbc9981194ff109
Paul Storm <paulwwstorm@gmail.com>
2025-08-06 21:57:11 -0500

    Brainstorming #77

--- LeetCode Top 150/Backtracking/SeventySeven.java
+++ LeetCode Top 150/Backtracking/SeventySeven.java
@@ -1,4 +1,7 @@
 class Solution {
+    //Basic idea is to generate all options
+    //Don't generate beyond limit
+    //Delete option that don't meet requirement
     public List<List<Integer>> combine(int n, int k) {
         
     }
