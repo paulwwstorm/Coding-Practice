76fa7334a7541a7262c772123cda721e86862d65
Paul Storm <paulwwstorm@gmail.com>
2025-08-19 15:01:01 -0500

    Basic solution, not working.

--- LeetCode Top 150/Divide and Conquer/OneHundredAndFortyEight.java
+++ LeetCode Top 150/Divide and Conquer/OneHundredAndFortyEight.java
@@ -9,7 +9,46 @@
  * }
  */
 class Solution {
+
+    public boolean bubbleSort(ListNode prev, boolean changed) {
+        ListNode curr = prev.next;
+        if (curr != null && curr.next != null) {
+            if (curr.next.val < curr.val) {
+                 prev.next = prev.next.next;
+                 prev.next.next = curr;
+                 changed = true;
+            }
+            
+            bubbleSort(prev.next, changed);
+        } else {
+            return changed;
+        }
+
+        return changed;
+    }
+
     public ListNode sortList(ListNode head) {
-        //Think the first option is to try to use a recursive bubble sort
+        boolean changed = true;
+
+        while (changed) {
+            changed = false;
+
+            ListNode curr = head;
+            if (curr != null && curr.next != null) {
+                if (curr.next.val < curr.val) {
+                    ListNode next = curr.next.next;
+                    head = curr.next;
+                    curr.next = curr;
+                    curr.next.next = next;
+                }
+                
+                bubbleSort(head, changed);
+            } 
+
+
+        }
+
+        return head;
+        
     }
 }
