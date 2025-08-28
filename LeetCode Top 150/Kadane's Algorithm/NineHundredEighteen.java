d7122c6bb9c75e1e0c142ad1c7971b8ba2581812
Paul Storm <paulwwstorm@gmail.com>
2025-08-28 09:30:09 -0500

    Is there a way to do this without at least one nested loop?

--- LeetCode Top 150/Kadane's Algorithm/NineHundredEighteen.java
+++ LeetCode Top 150/Kadane's Algorithm/NineHundredEighteen.java
@@ -1,13 +1,16 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
         int max = Integer.MIN_VALUE;
+        int[] combined = new int[nums.length*2];
+        System.arraycopy(nums, 0, combined, 0, nums.length);
+        System.arraycopy(nums, 0, combined, nums.length, nums.length);
         int curr = 0;
-        int numsLen = nums.length;
+        int numsLen = combined.length;
         int subArrayLen = 0;
 
-        for (int i = 0; i < numsLen*2; i++) {
+        for (int i = 0; i < numsLen; i++) {
 
-            curr += nums[i % numsLen];
+            curr += combined[i];
             subArrayLen++;
 
             if (curr > max) {
@@ -19,9 +22,19 @@ class Solution {
                 subArrayLen = 0;
             }
 
-            if (subArrayLen+1 == numsLen) {
-                curr = (nums[i % numsLen] > 0) ? nums[i % numsLen] : 0;
-                subArrayLen = (nums[i % numsLen] > 0) ? 1 : 0;
+            if (subArrayLen >= nums.length) {
+                System.out.println(String.valueOf(curr));
+                curr = curr-combined[i-(nums.length) + 1];
+                System.out.println(String.valueOf(combined[i-(nums.length) + 1]));
+
+                if (curr > max) {
+                    max = curr;
+                }
+
+                subArrayLen--;
+
+                // curr = (combined[i % numsLen] > 0) ? combined[i % numsLen] : 0;
+                // subArrayLen = (combined[i % numsLen] > 0) ? 1 : 0;
             }
 
         }
