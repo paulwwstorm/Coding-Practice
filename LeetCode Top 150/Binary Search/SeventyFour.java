43322e94156f5b8b000375033fced70cdba136c0
Paul Storm <paulwwstorm@gmail.com>
2025-09-05 15:58:25 +0200

    Actually pretty easy overall

--- LeetCode Top 150/Binary Search/SeventyFour.java
+++ LeetCode Top 150/Binary Search/SeventyFour.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
+        for (int i = 0; i < matrix.length; i++) {
+            int row = i;
+            if (target >= matrix[row][0] && target <= matrix[row][matrix[0].length - 1]) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (matrix[row][j] == target) {
+                        return true;
+                    }
+                }
+            } else if (target < matrix[row][0]) {
+                return false;
+            }
+        }
+
+        return false;
     }
 }
