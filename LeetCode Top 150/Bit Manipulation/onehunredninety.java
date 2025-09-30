201ecdc9dbd4f7fb4b972d7c7798db10b94cb9e7
Paul Storm <paulwwstorm@gmail.com>
2025-09-30 19:54:58 +0200

    Need to learn a bit about bits.

--- LeetCode Top 150/Bit Manipulation/onehunredninety.java
+++ LeetCode Top 150/Bit Manipulation/onehunredninety.java
@@ -1,5 +1,18 @@
 class Solution {
     public int reverseBits(int n) {
-        
+        String binaryString = Integer.toBinaryString(n);
+        String binaryStringReverse = "";
+
+        System.out.println(binaryString);
+
+
+        for (int i = 0; i < binaryString.length(); i++) {
+            binaryStringReverse = binaryString.charAt(i) + binaryStringReverse;
+        }
+
+        System.out.println(binaryStringReverse);
+
+        int reverse = Integer.parseInt(binaryStringReverse, 2);
+        return reverse;
     }
 }
