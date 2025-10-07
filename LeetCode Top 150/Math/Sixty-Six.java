ecb6faf683bbe61ad3444b4dde663a22d096f804
Paul Storm <paulwwstorm@gmail.com>
2025-10-07 10:02:38 +0200

    Always fun when something so simple requires so much code

---
+++ LeetCode Top 150/Math/Sixty-Six.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] increment(int[] digits, int loc) {
+        if (digits[loc] < 9) {
+            digits[loc] += 1;
+        } else if (loc > 0) {
+            digits[loc] = 0;
+            digits = increment(digits, loc--);
+        } else {
+            int[] expandedDigits = new int[digits.length + 1];
+            expandedDigits[0] = 1;
+            expandedDigits[1] = 0;
+            for (int i = 1; i < digits.length; i++) {
+                expandedDigits[i+1] = digits[i];
+            }
+
+            digits = expandedDigits;
+        }
+
+        return digits;
+    }
+    public int[] plusOne(int[] digits) {
+        digits = increment(digits, digits.length-1);
+        return digits;
+    }
+}
