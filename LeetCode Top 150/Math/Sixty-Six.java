9f3a5e210afb91db3677dfb0f53366056e70f62b
Paul Storm <paulwwstorm@gmail.com>
2025-10-08 09:01:33 +0200

    Solved #66 and learned about order of operations.

--- LeetCode Top 150/Math/Sixty-Six.java
+++ LeetCode Top 150/Math/Sixty-Six.java
@@ -4,7 +4,8 @@ class Solution {
             digits[loc] += 1;
         } else if (loc > 0) {
             digits[loc] = 0;
-            digits = increment(digits, loc--);
+            loc--;
+            digits = increment(digits, loc);
         } else {
             int[] expandedDigits = new int[digits.length + 1];
             expandedDigits[0] = 1;
